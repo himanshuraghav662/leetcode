fc7eddd2478e4f66573db4283a0728363b47e895
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2023-08-05 23:15:46 +0530

    Update 283 move zeros.java

--- 283 move zeros.java
+++ 283 move zeros.java
@@ -1,4 +1,22 @@
-class Solution {
+class Solution {                                //1ms
+    public void moveZeroes(int[] nums) {
+        int q=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[q]=nums[i];
+                q++;
+            }
+        }
+        while(q<nums.length){
+            nums[q]=0;
+            q++;
+        }
+    }
+}
+
+                            
+
+class Solution {                                //2ms
     public void moveZeroes(int[] nums) {
         int c=0;
         for(int i=0;i<nums.length;i++){
