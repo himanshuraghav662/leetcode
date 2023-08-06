3da1c8eb04d541996ddb1a0da2b5c07f1bc23b9f
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2023-08-06 22:13:47 +0530

    Update problem41.java

--- problem41.java
+++ problem41.java
@@ -1,4 +1,4 @@
-
+                         //using hash table
 class Solution {
     public int firstMissingPositive(int[] nums) {
         HashSet<Integer> list=new HashSet<Integer>();
@@ -15,5 +15,37 @@ class Solution {
             }
         }
         return(i+1);
+    }
+}
+
+
+
+                               //using cycle sort
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+         int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        int count = 0;
+
+         for(int j = 0; j < nums.length; j++) {
+
+            if(nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+        return nums.length+1;
     }
 }
