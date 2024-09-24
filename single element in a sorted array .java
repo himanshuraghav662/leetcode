78bb275b9a7401d5f18686c03a67f4c6b58840c8
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-09-24 18:02:16 +0530

    Create single element in a sorted array .java

---
+++ single element in a sorted array .java
@@ -0,0 +1,15 @@
+## binary search##
+
+  class Solution {
+    public int singleNonDuplicate(int[] nums) {
+       int start=0;
+       int end=nums.length-1;
+       while(start < end){
+        int mid=(start+end)/2;
+        if(mid % 2 == 1) mid--;
+        if(nums[mid]!=nums[mid+1]) end=mid;
+        else start=mid+2;
+       }
+     return nums[end];  
+    }
+}
